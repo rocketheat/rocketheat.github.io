b3a271c276bdc951de73a59d700c4150c7ca1fd7
rocketheat <33535126+rocketheat@users.noreply.github.com>
2024-11-03 22:07:18 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/global-spine-vector-web/",
-  resolve: {
-    alias: {
-      "node-fetch": "isomorphic-fetch",
-    },
-  },
+  base: '/global-spine-vector-web/', // Adjust this to match your GitHub repo name
+  build: {
+    outDir: 'dist'
+  }
 });
